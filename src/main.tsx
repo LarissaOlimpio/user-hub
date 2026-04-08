fce8742d1cee3fb46e694f3caf6d22c19b99461e
larissaolimpio <lrsolimpio@gmail.com>
2026-04-08 19:04:15 -0300

    feat: config tanstack query and mui materials in main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,31 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const queryClient = new QueryClient();
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#007FFF",
+    },
+    background: {
+      default: "#f8fafc",
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
